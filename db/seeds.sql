27cf63c109feb6167a4c35dabaf88b793014386a
Sergieo <sergieol23@gmail.com>
2022-04-24 01:37:11 -0500

    Added db schema and seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+    ('Sergieo', 'Lopez', 1),
+    ('Sergy', 'Woolf', 1),
+    ('Sergina', 'Gaveston', 0),
+    ('Serg', 'LeRoi', 1),
+    ('Mr. Sergieo', 'Mansfield', 1),
+    ('The Sergieo', 'Carrington', 0),
+    ('Sergy Serg', 'Bellamy', 0),
+    ('Sir Sergieo', 'Summers', 1),
+    ('Sir Gee Oh', 'Butler', 1),
+    ('Sersio', 'Zurn', 1);
